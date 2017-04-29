639fa560a51ff28531315819416355c7b7f0418f
HasanKarilov <hkarilov@gmail.com>
2017-04-29 16:28:57 +0600

    CommressTiles & MergeTiles 05

--- Model.java
+++ Model.java
@@ -9,6 +9,8 @@ import java.util.List;
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
+    int score;
+    int maxTile;
 
     public Model(){
         resetGameTiles();
@@ -26,6 +28,8 @@ public class Model {
                 gameTiles[i][j] = new Tile();
             }
         }
+        score = 0;
+        maxTile = 2;
         addTile();
         addTile();
     }
@@ -54,6 +58,38 @@ public class Model {
             tiles.get( (int) (tiles.size() * Math.random()) )
                     .value = Math.random() < 0.9 ? 2 : 4;
         }
+    }
+
+    /**
+     *
+     */
+    private boolean compressTiles(Tile[] tiles) {
+        boolean change = false;
+        for (int k = 0; k < tiles.length - 1; k++) {
+            for (int i = 0; i < tiles.length - 1; i++) {
+                if (tiles[i].isEmpty() && !tiles[i+1].isEmpty()) {
+                    change = true;
+                    tiles[i] = tiles[i+1];
+                    tiles[i+1] = new Tile();
+                }
+            }
+        }
+        return change;
+    }
+
+    private boolean mergeTiles(Tile[] tiles) {
+        boolean change = false;
+        for (int i = 0; i < tiles.length - 1; i++) {
+            if (tiles[i].value == tiles[i+1].value && !tiles[i].isEmpty() && !tiles[i+1].isEmpty()) {
+                change = true;
+                tiles[i].value *= 2;
+                tiles[i+1] = new Tile();
+                maxTile = maxTile > tiles[i].value ? maxTile : tiles[i].value;
+                score += tiles[i].value;
+                compressTiles(tiles);
+            }
+        }
+        return change;
     }
 
 }
