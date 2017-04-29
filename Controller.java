a4557300126d01fddd44d2862878e34c62603701
HasanKarilov <hkarilov@gmail.com>
2017-04-29 13:40:43 +0600

    All classes we need 01

---
+++ Controller.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Следит за нажитием клавиш во время игры
+ */
+public class Controller {
+}
---
+++ View.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Обеспечит отображение текущего сотояния игры
+ */
+public class View {
+}
