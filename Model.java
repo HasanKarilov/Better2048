4b27f1054da3782c8100356d13116e19e825ed3e
HasanKarilov <hkarilov@gmail.com>
2017-04-29 14:17:43 +0600

    Platform initialization on Model 03

--- Model.java
+++ Model.java
@@ -4,4 +4,18 @@ package com.javarush.task.task35.task3513;
  * Будет содержать игровую логику и хранить игровое поле
  */
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model(){
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < FIELD_WIDTH; i++)
+        {
+            for (int j = 0; j < FIELD_WIDTH; j++)
+            {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
+
 }
